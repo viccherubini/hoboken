fed7535068879c1a29c1b3b88d5f2edf779daf69
Vic Cherubini <vmc@leftnode.com>
2010-06-28 13:10:16 -0500

    Better example.

--- example/index.php
+++ example/index.php
@@ -6,22 +6,31 @@ require_once 'Hoboken/Hoboken.php';
 require_once 'Hoboken/Exception.php';
 
 $hoboken = new \Hoboken;
-$hoboken->layout = 'hoboken';
+$hoboken->layout = 'leftnode';
 $hoboken->layoutDirectory = 'public/layout/';
 $hoboken->viewDirectory = 'public/view/';
 
 try {
 
 	$hoboken->GET('/', function($self) {
-		echo 'hi there';
+		$self->blah = "some value";
 	}, 'index');
 
-
-	$hoboken->GET('/hire-me', function() {
-		
+	$hoboken->GET('/hire-me/%n', function($self, $id) {
+		$self->id = $id;
 	}, 'hire-me');
-
-} catch ( HobokenException $e ) {
 	
+	$hoboken->GET('/no-layout', function($self) {
+		$self->layout = NULL;
+	}, 'no-layout');
+	
+	$hoboken->POST('/hire-me', function($self) {
+		$self->layout = NULL;
+		echo 'you want to hire me by posting me';
+	});
+
+	echo $hoboken->execute();
+	
+} catch ( HobokenException $e ) {
 	echo $e->getMessage();
 }
