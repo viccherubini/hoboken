b6a0c9c6f0fb6265b9ca0b64a87e49db1d6b69b7
Vic Cherubini <vmc@leftnode.com>
2010-06-28 07:32:43 -0500

    Added all of the code for routing.

--- Hoboken/Hoboken.php
+++ Hoboken/Hoboken.php
@@ -4,6 +4,7 @@ declare(encoding='UTF-8');
 
 class Hoboken {
 	
+	private $actionArgv = array();
 	private $actionList = array();
 	private $extractList = array();
 	private $routeSource = array();
@@ -11,6 +12,7 @@ class Hoboken {
 
 	private $layout = NULL;
 	private $layoutDirectory = NULL;
+	
 	private $viewDirectory = NULL;
 	
 	
@@ -63,6 +65,143 @@ class Hoboken {
 		$this->serverParams = $server;
 		return $this;
 	}
+	
+	public function GET($route, $action, $view = NULL) {
+		$this->addRouteAndAction(self::METHOD_GET, $route, $action, $view);
+		return $this;
+	}
+	
+	public function POST($route, $action, $view = NULL) {
+		$this->addRouteAndAction(self::METHOD_POST, $route, $action, $view);
+		return $this;
+	}
+	
+	
+	
+	
+	
+	
 
+	public function isValidRoute($route) {
+		/* Special case of a valid route. */
+		if ( '/' == $route ) {
+			return true;
+		}
+		
+		/**
+		 * Hoboken is more restrictive about routes than the normal Internet RFC
+		 * standards. This is to keep them clean, legible and readible.
+		 */
+		if ( 0 === preg_match('#^/([a-z]+)([a-z0-9_\-/%\.]*)$#i', $route) ) {
+			return false;
+		}
+		
+		return true;
+	}
 
+	public function canRouteUri($route, $uri) {
+		/* Remove the beginning / from the URI and route. */
+		$uri = ltrim($uri, '/');
+		$uriChunkList = explode('/', $uri);
+		$uriChunkCount = count($uriChunkList);
+		
+		$route = ltrim($route, '/');
+		$routeChunkList = explode('/', $route);
+		$routeChunkCount = count($routeChunkList);
+		
+		/* If all of the chunks eventually match, we have a matched route. */
+		$matchedChunkCount = 0;
+
+		/* List of arguments to pass to the action method. */
+		$argv = array();
+
+		if ( $uriChunkCount === $routeChunkCount ) {
+			for ( $i=0; $i<$routeChunkCount; $i++ ) {
+				/* ucv == uri chunk value */
+				$ucv = $uriChunkList[$i];
+				
+				/* rcv = route chunk value */
+				$rcv = $routeChunkList[$i];
+
+				if ( $ucv == $rcv ) {
+					/* If the two are exactly the same, no expansion is needed. */
+					$matchedChunkCount++;
+				} else {
+					/**
+					 * More investigation is required. See if there is a % character followed by a (n|s), and if so, expand it.
+					 * A limitation is that only a single % replacement can exist in a chunk at once, for now.
+					 * 
+					 * @todo Allow multiple %n or %s characters in a chunk at once.
+					 */
+					$offset = stripos($rcv, '%');
+					if ( false !== $offset && true === isset($rcv[$offset+1]) ) {
+						$rcvType = $rcv[$offset+1];
+						$rcvLength = strlen($rcv);
+						
+						if ( 0 !== $offset ) {
+							$ucv = trim(substr_replace($ucv, NULL, 0, $offset));
+						}
+						
+						if ( ($offset+2) < $rcvLength ) {
+							$goto = strlen(substr($rcv, $offset+2));
+							$ucv = substr_replace($ucv, NULL, -$goto);
+						}
+						
+						/* Now that we have the correct $ucv values, let's make sure they're types are correct. */
+						$matched = false;
+						
+						switch ( $rcvType ) {
+							case 'n': {
+								$matched = is_numeric($ucv);
+								break;
+							}
+							
+							case 's': {
+								$matched = is_string($ucv) && !is_numeric($ucv);
+								break;
+							}
+						}
+						
+						if ( true === $matched ) {
+							$matchedChunkCount++;
+							$argv[$rcv] = $ucv;
+						}
+					}
+				}
+			}
+		}
+		
+		$this->argv = $argv;
+	
+		return ( $matchedChunkCount === $routeChunkCount );
+	}
+	
+	private function addRouteAndAction($requestMethod, $route, $action, $view) {
+		$this->hasValidRoute($route);
+		$this->hasClosure($action);
+		
+		$routeObject = new \stdClass;
+		$routeObject->route = $route;
+		$routeObject->action = $action;
+		$routeObject->view = $view;
+		
+		$this->actionList[$requestMethod][] = $routeObject;
+		
+	}
+	
+	private function hasValidRoute($route) {
+		if ( !$this->isValidRoute($route) ) {
+			throw new \Exception("The route {$route} is invalid and can not be properly translated.");
+		}
+	}
+	
+	private function isClosure($closure) {
+		return ( $closure instanceof \Closure );
+	}
+	
+	private function hasClosure($closure) {
+		if ( !$this->isClosure($closure) ) {
+			throw new \Exception("The action is invalid because it is not a closure.");
+		}
+	}
 }
