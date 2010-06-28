77ee287824c008e644a07ad3bbc84a2ed0161405
Vic Cherubini <vmc@leftnode.com>
2010-06-28 05:50:29 -0500

    Added the HobokenTest suite.

--- Testsuite/AllTests.php
+++ Testsuite/AllTests.php
@@ -3,12 +3,14 @@
 declare(encoding='UTF-8');
 namespace HobokenTest;
 
+require_once 'HobokenTest.php';
 require_once 'MiscTest.php';
 
 class AllTests {
 	public static function suite() {
 		$suite = new \PHPUnit_Framework_TestSuite('Hoboken Tests');
 
+		$suite->addTestSuite('\HobokenTest\HobokenTest');
 		$suite->addTestSuite('\HobokenTest\MiscTest');
 		
 		return $suite;
