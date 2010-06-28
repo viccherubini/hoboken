cc1abb673a9fcb15a60bd627e014e994cf43e358
Vic Cherubini <vmc@leftnode.com>
2010-06-28 16:31:37 -0500

    Made a central include file for all your Hoboken goodness.

--- example/index.php
+++ example/index.php
@@ -2,8 +2,7 @@
 
 declare(encoding='UTF-8');
 
-require_once 'Hoboken/Hoboken.php';
-require_once 'Hoboken/Exception.php';
+require_once 'Hoboken/Framework.php';
 
 $hoboken = new \Hoboken;
 $hoboken->layout = 'leftnode';
---
+++ lib/Framework.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(encoding='UTF-8');
+
+require_once 'Hoboken/Hoboken.php';
+require_once 'Hoboken/Exception.php';
