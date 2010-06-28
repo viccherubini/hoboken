3c1112225db388e2947c7a4a38655d3bc4d5be87
Vic Cherubini <vmc@leftnode.com>
2010-06-28 05:51:02 -0500

    Added method assertHoboken() to check if a value is an instance of Hoboken.

--- Testsuite/TestCase.php
+++ Testsuite/TestCase.php
@@ -3,19 +3,26 @@
 declare(encoding='UTF-8');
 namespace HobokenTest;
 
+use \Hoboken;
+
 class TestCase extends \PHPUnit_Framework_TestCase {
 	
-	public static function assertArray($a, $message = '') {
+	public static function assertArray($a, $message = NULL) {
 		self::assertThat(is_array($a), self::isTrue(), $message);
 	}
 	
-	public static function assertEmptyArray($a, $message = '') {
+	public static function assertEmptyArray($a, $message = NULL) {
 		self::assertArray($a);
 		self::assertEquals(0, count($a), $message);
 	}
 	
-	public static function assertNotEmptyArray($a, $message = '') {
+	public static function assertNotEmptyArray($a, $message = NULL) {
 		self::assertArray($a, $message);
 		self::assertGreaterThan(0, count($a), $message);
+	}
+	
+	public static function assertHoboken($obj, $message = NULL) {
+		self::assertTrue(is_object($obj));
+		self::assertTrue($obj instanceof \Hoboken);
 	}
 }
